bfffea3844d602916522fc85f170181f3ac0e9d2
datsciseol <dat.sci.seol@gmail.com>
2020-04-17 09:01:53 +0900

    done

---
+++ 0417/udemy/pdf.js
@@ -0,0 +1,30 @@
+// Numbers
+4
+9.3
+-10
+
+// Strings
+"Hello World"
+"43"
+
+// Booleans
+true
+false
+
+// null and undefined
+null
+undefiend
+
+// Variables are simply containers that store values
+// They follow this pattern:
+var yourVariableName = yourValue;
+
+// Null and Undefiend
+The two other primitives are null and undefined
+Variables that are declared but not initialized are undefined
+The following varialbes are undefined:
+var name;
+var age;
+null is "explicitly nothing"
+var currentPlayer = "charlie";
+currentPlayer = null;
---
+++ 0417/udemy/pdf2.js
@@ -0,0 +1,6 @@
+# Boolean Logic
+Everything starts with the idea that a statement is either True or false.value
+In JS, double equal sign is more harsh than others
+let x = 5;
+x == "5" : True(only value concerned)
+x === '5' : false(type and value concerend)
