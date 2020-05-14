e6ab4f8607541d0147cfe43b55e2827e3ca87a02
datsciseol <dat.sci.seol@gmail.com>
2020-05-14 16:54:27 +0900

    udemy dom

---
+++ 0514/toggle.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('button');
+var isPurple = false;
+
+button.addEventListener("click", function(){
+    document.body.classList.toggle("purple");
+    // if white
+    //  make it purple
+    // else
+    //  make it white
+});
